92c073d80058d381c1689a3ddb85c2bb72fd440f
QuintipleAce <Ayanparker278@gmail.com>
2026-08-11 21:50:14 -0400

    future proofed code for new additions with cleaner code

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,22 +3,37 @@ extends CharacterBody2D
 
 var speed: int = 300
 var dir: Vector2 = Vector2.ZERO
-var bullet_scene = preload("res://Scenes/bullet.tscn")
-var bullet: Node2D
+var last_dir: Vector2 = Vector2.RIGHT
+var projectile_scene = preload("res://Scenes/bullet.tscn")
+var projectile: Node2D
 
 func _physics_process(delta: float) -> void:
 	dir = Input.get_vector("left", "right", "up", "down").normalized()
 	velocity = dir * speed
 	move_and_slide()
+	direction()
+	shoot()
 	
-	if Input.is_action_just_pressed("action"):
-		bullet = bullet_scene.instantiate()
-		shoot(bullet, get_local_mouse_position())
-		
 
-func shoot(bullet: Area2D, mouse_pos: Vector2):
-	bullet.position = position
-	bullet.rotation = mouse_pos.angle()
-	bullet.dir = mouse_pos.normalized()
-	get_tree().current_scene.find_child("Bullets").add_child(bullet)
-	print("helloo")
+func shoot():
+	
+	if Input.is_action_just_pressed("KBMaction"):
+		var mouse_pos = get_local_mouse_position()
+		projectile = projectile_scene.instantiate()
+		projectile.position = position
+		projectile.rotation = mouse_pos.angle()
+		projectile.dir = mouse_pos.normalized()
+		get_tree().current_scene.find_child("Projectiles").add_child(projectile)
+
+	if Input.is_action_just_pressed("CONaction"):
+		projectile = projectile_scene.instantiate()
+		projectile.position = position
+		projectile.rotation = $MeshInstance2D.rotation
+		projectile.dir = last_dir
+		get_tree().current_scene.find_child("Projectiles").add_child(projectile)
+	
+	
+func direction():
+	if !dir == Vector2.ZERO:
+		$MeshInstance2D.rotation = dir.angle()
+		last_dir = dir
